c0a01ab81354ea5f0daf3aa65dd34e0000079422
Victor Marques <victorfmarques@outlook.com>
2021-09-11 17:33:00 -0300

    Aula 2 - Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
+void main() =>
+    runApp(
       MaterialApp(
         home: Scaffold(
-          body: Column(
-            children: [
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text('Titulo do Item'),
-                  subtitle: Text('Subtitulo do Item'),
-                ),
-              ),Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text('Titulo do Item'),
-                  subtitle: Text('Subtitulo do Item'),
-                ),
-              ),Card(
-                child: ListTile(
-                  leading: Icon(Icons.monetization_on),
-                  title: Text('Titulo do Item'),
-                  subtitle: Text('Subtitulo do Item'),
-                ),
-              ),
-            ],
-          ),
+          body: ListaTransferencias(),
           appBar: AppBar(title: Text('Transferências')),
         ),
       ),
     );
+
+class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(Transferencia(100.90, 1)),
+        ItemTransferencia(Transferencia(99.90, 12)),
+        ItemTransferencia(Transferencia(10.90, 13)),
+        ItemTransferencia(Transferencia(1.99, 41)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+  Transferencia _transferencia;
+
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
