9bc553c47a60a4761a8752fbba21d2788f190aa0
Victor Marques <victorfmarques@outlook.com>
2021-09-11 17:12:50 -0300

    Aula 2 Parte 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,32 @@
 import 'package:flutter/material.dart';
 
-void main() => (runApp(Text('Bem vindo ao Bytebank', textDirection: TextDirection.ltr)));
-
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          body: Column(
+            children: [
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('Titulo do Item'),
+                  subtitle: Text('Subtitulo do Item'),
+                ),
+              ),Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('Titulo do Item'),
+                  subtitle: Text('Subtitulo do Item'),
+                ),
+              ),Card(
+                child: ListTile(
+                  leading: Icon(Icons.monetization_on),
+                  title: Text('Titulo do Item'),
+                  subtitle: Text('Subtitulo do Item'),
+                ),
+              ),
+            ],
+          ),
+          appBar: AppBar(title: Text('Transferências')),
+        ),
+      ),
+    );
